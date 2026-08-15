239022770e5e0c19a10762132d028b2b7cbe48ea
Tit Lymeng <titlymeng720@gmail.com>
2026-08-15 11:43:20 +0700

    backend/todolist

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,7 @@ const app = express();
 // app.use(express.json());
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin: ['https://frontend-todolist-lu9pahjgz-tit-limengs-projects.vercel.app/','http://localhost:5173','http://localhost:3000','http://192.168.1.9:5173'],
+    origin: ['https://frontend-todolist-lu9pahjgz-tit-limengs-projects.vercel.app','http://localhost:5173','http://localhost:3000','http://192.168.1.9:5173'],
     credentials: true,
 }))
 
