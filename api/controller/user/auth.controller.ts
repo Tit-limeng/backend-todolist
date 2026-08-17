1617128a88dd05153320d108186b773237769d8f
Tit Lymeng <titlymeng720@gmail.com>
2026-08-17 15:34:42 +0700

    backend/todolist

--- api/controller/user/auth.controller.ts
+++ api/controller/user/auth.controller.ts
@@ -97,14 +97,12 @@ export const login = async (req: Request, res: Response) => {
         console.log("this is token in cookie", req.cookies.token);
         const access_token = jwt.sign({ id: user.user_id, role: user.role }, token, { expiresIn: '30d' });
         const { password: _, ...userData } = user;
-        const isProduction = process.env.NODE_ENV === "production";
-        
         res.cookie("token", access_token, {
-            httpOnly: true,
-            sameSite: isProduction ? "none" : "lax",
-            secure: isProduction,
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-            // maxAge : 1 * 60 * 1000 ,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: "/",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
         });
 
         res.status(200).json({
@@ -225,12 +223,11 @@ export const googleLogin = async (
 
 
     // Save cookie
-        const isProduction = process.env.NODE_ENV === "production";
-    res.cookie("token", access_token, {
+       res.cookie("token", access_token, {
       httpOnly: true,
-      secure: isProduction,
-    //   sameSite: "none",
-    sameSite: isProduction ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
+      path: "/",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
@@ -254,148 +251,6 @@ export const googleLogin = async (
 };
 
 
-// const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
-// export const googleLogin = async (
-//   req: Request,
-//   res: Response
-// ): Promise<void> => {
-//   try {
-//     const googleClientId = process.env.GOOGLE_CLIENT_ID;
-//     const jwtSecret = process.env.JWT_SECRET_KEY;
-
-//     if (!googleClientId || !jwtSecret) {
-//       res.status(500).json({
-//         message: "Missing environment variables",
-//         error: true,
-//       });
-//       return;
-//     }
-
-//     const { credential } = req.body;
-
-//     if (!credential) {
-//       res.status(400).json({
-//         message: "Google credential is required",
-//         error: true,
-//       });
-//       return;
-//     }
-
-//     // Verify Google Token
-//     const ticket = await client.verifyIdToken({
-//       idToken: credential,
-//       audience: googleClientId,
-//     });
-
-//     const payload = ticket.getPayload();
-
-//     if (!payload) {
-//       res.status(401).json({
-//         message: "Invalid Google token",
-//         error: true,
-//       });
-//       return;
-//     }
-
-//     const {
-//       email,
-//       name,
-//       sub,
-//       email_verified,
-//     } = payload;
-
-//     if (!email || !email_verified) {
-//       res.status(401).json({
-//         message: "Google email is not verified",
-//         error: true,
-//       });
-//       return;
-//     }
-
-//     // Create user if doesn't exist, otherwise update Google ID
-//     await pool.query(
-//       `
-//       INSERT INTO users
-//       (
-//         username,
-//         email,
-//         google_id,
-//         role_id,
-//         login
-//       )
-//       VALUES
-//       (
-//         $1,
-//         $2,
-//         $3,
-//         $4,
-//         $5
-//       )
-//       ON CONFLICT (email)
-//       DO UPDATE
-//       SET
-//         username = EXCLUDED.username,
-//         google_id = EXCLUDED.google_id
-//       `,
-//       [
-//         name,
-//         email,
-//         sub,
-//         2,      // Default Role
-//         true,   // Active account
-//       ]
-//     );
-
-//     // Get user with role
-//     const result = await pool.query(
-//       `
-//       SELECT
-//         users.*,
-//         roles.role
-//       FROM users
-//       INNER JOIN roles
-//       ON users.role_id = roles.role_id
-//       WHERE users.email = $1
-//       `,
-//       [email]
-//     );
-
-//     const user = result.rows[0];
-
-//     const access_token = jwt.sign(
-//       {
-//         id: user.user_id,
-//         role: user.role,
-//       },
-//       jwtSecret,
-//       {
-//         expiresIn: "30d",
-//       }
-//     );
-
-//     res.cookie("token", access_token, {
-//       httpOnly: true,
-//       secure: false, // true in production with HTTPS
-//       sameSite: "lax",
-//       maxAge: 30 * 24 * 60 * 60 * 1000,
-//     });
-
-//     res.status(200).json({
-//       message: "Login successful",
-//       error: false,
-//       access_token,
-//       user,
-//     });
-//   } catch (error) {
-//     console.error("Google Login Error:", error);
-
-//     res.status(500).json({
-//       message: "Internal server error",
-//       error: true,
-//     });
-//   }
-// };
 
 export const getUser = async (req: Request, res: Response) => {
     try {
